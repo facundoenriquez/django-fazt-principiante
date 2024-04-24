7837e77461f520b76be1f6036d5214f094a999a6
Facundo Enriquez <fenriquez@inym.org.ar>
2024-04-24 13:52:05 -0300

    end django para principiante

---
+++ myapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class CreateNewTask(forms.Form):
+    title = forms.CharField(label="Task title", max_length=200)
+    description = forms.CharField(
+        label="Task description", widget=forms.Textarea)
+
+
+class CreateNewProject(forms.Form):
+    name = forms.CharField(label="New Project Name", max_length=200)
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
+from .forms import CreateNewTask, CreateNewProject
 # Create your views here.
 
 
@@ -25,11 +26,35 @@ def about(request):
 def projects(request):
     # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html',{
+    return render(request, 'projects/projects.html', {
         'projects': projects
     })
 
 
-def tasks(request, id):
-    task = get_object_or_404(Task, id=id)
-    return render(request, 'tasks.html')
+def tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/tasks.html', {
+        'tasks': tasks
+    })
+
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'tasks/create_task.html', {
+            'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(
+            title=request.POST['title'], description=request.POST['description'], project_id=2)
+        return redirect('tasks')
+
+
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create_project.html', {
+            'form': CreateNewProject()
+        })
+    else:
+        Project.objects.create(
+            name=request.POST['name'])
+        return redirect('projects')
